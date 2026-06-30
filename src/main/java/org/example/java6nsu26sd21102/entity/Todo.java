27aec05f8bf9c469ce171d909e0f38b6e6121fe0
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-06-30 20:02:13 +0700

    API basics: using JPA to create todos table of Todo entity

---
+++ src/main/java/org/example/java6nsu26sd21102/entity/Todo.java
@@ -0,0 +1,21 @@
+package org.example.java6nsu26sd21102.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+}
