28f80d8945d74ec1045a4f8f5c40dd914ae02a3a
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-07-14 20:09:30 +0700

    API basics: using ResponseEntity

--- src/main/java/org/example/java6nsu26sd21102/controller/TodoController.java
+++ src/main/java/org/example/java6nsu26sd21102/controller/TodoController.java
@@ -3,6 +3,8 @@ package org.example.java6nsu26sd21102.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.java6nsu26sd21102.entity.Todo;
 import org.example.java6nsu26sd21102.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,37 +18,49 @@ public class TodoController {
 
     // api/todos
     @GetMapping
-    public List<Todo> getAllTodos() {
+    public ResponseEntity<List<Todo>> getAllTodos() {
 
-        return todoService.findAll();
+        List<Todo> todos = todoService.findAll();
+
+        return new ResponseEntity<>(todos, HttpStatus.OK);
+
+        //return ResponseEntity.ok(todos);
+
+        //return ResponseEntity.status(HttpStatus.OK).body(todos);
+
+        //return ResponseEntity.ok().header("Custom-Header", "Custom-Value").body(todos);
     }
 
     // api/todos/id
     @GetMapping("{id}")
-    public Todo getTodo(@PathVariable long id) {
+    public ResponseEntity<Todo> getTodo(@PathVariable long id) {
 
-        return todoService.findById(id);
+        Todo todo = todoService.findById(id);
+        return new ResponseEntity<>(todo, HttpStatus.OK);
     }
 
     // api/todos
     @PostMapping
-    public Todo addTodo(@RequestBody Todo todo) {
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
 
-        return todoService.add(todo);
+        Todo savedTodo = todoService.add(todo);
+        return new ResponseEntity<>(savedTodo, HttpStatus.CREATED);
     }
 
     // api/todos/id
     @PutMapping("{id}")
-    public Todo updateTodo(@RequestBody Todo todo, @PathVariable long id) {
+    public ResponseEntity<Todo> updateTodo(@RequestBody Todo todo, @PathVariable long id) {
 
-        return todoService.update(todo, id);
+        Todo updatedTodo = todoService.update(todo, id);
+        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
     }
 
     // api/todos/id
     @DeleteMapping("{id}")
-    public Todo deleteTodo(@PathVariable long id) {
+    public ResponseEntity<Todo> deleteTodo(@PathVariable long id) {
 
-        return todoService.delete(id);
+        Todo deletedTodo = todoService.delete(id);
+        return new ResponseEntity<>(deletedTodo, HttpStatus.OK);
     }
 
 }
