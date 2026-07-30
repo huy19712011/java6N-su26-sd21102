e21ea1dfa3263f5240f08aa473e16d910bf288ad
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-07-30 20:07:14 +0700

    Security: default; using application.properties; in memory

---
+++ src/main/java/org/example/java6nsu26sd21102/config/SpringSecurityConfig.java
@@ -0,0 +1,56 @@
+package org.example.java6nsu26sd21102.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails customer = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("123456"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(customer, admin);
+
+
+    }
+}
